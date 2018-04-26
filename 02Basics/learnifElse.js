439f6dc236550a3f45669aa2f0f3275b4542fff4
haughtypunk <kevin.xarro@gmail.com>
2018-04-27 01:04:17 +0530

    ifElse Application

--- 02Basics/learnifElse.js
+++ 02Basics/learnifElse.js
@@ -1,8 +1,21 @@
-if(false){
-    console.log('i am inside IF Block')
-    console.log('I am still inside the Block')
-} else if(true){
-    console.log('I am inside ELSE IF Block')
-}   else {
-    console.log('I am inside ELSE Block')
+// if(false){
+//     console.log('i am inside IF Block')
+//     console.log('I am still inside the Block')
+// } else if(true){
+//     console.log('I am inside ELSE IF Block')
+// }   else {
+//     console.log('I am inside ELSE Block')
+// }
+
+var whoIsHere = 'IronMan'
+
+if(whoIsHere === 'user'){
+    console.log('Greeting message for user')
+    console.log('Allow access to view all courses')
+} else if (whoIsHere === 'teacher'){
+    console.log('Greeting message for Teacher')
+    console.log('Allow access to his courses.')
+} else{
+    console.log('MESSAGE: Please verify your email')
+    console.log('Please send a message to user for verification.')
 }
