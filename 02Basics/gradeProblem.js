9f5671e94820c3d5208def765c7768321a7eb655
haughtypunk <kevin.xarro@gmail.com>
2018-04-27 01:10:30 +0530

    Grade Problem Solved!

---
+++ 02Basics/gradeProblem.js
@@ -0,0 +1,22 @@
+// Grade Problem
+
+// student marks :
+
+// 10 -Amazing
+// 5 - Good
+// 3 - Poor
+// 0 - Fail
+
+var marks = 10;
+
+if(marks === 10){
+    console.log('Amazing')
+} else if(marks === 5){
+    console.log('Good')
+} else if(marks === 3){
+    console.log('Poor')
+} else if(marks === 0){
+    console.log('Fail')
+} else {
+    console.log('Blah!!')
+}
