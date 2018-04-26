0adf5034ba8b8ba65b1635ebc3fab2e0c1f10f46
haughtypunk <kevin.xarro@gmail.com>
2018-04-27 00:55:43 +0530

    learnIfElse

---
+++ 02Basics/learnifElse.js
@@ -0,0 +1,8 @@
+if(false){
+    console.log('i am inside IF Block')
+    console.log('I am still inside the Block')
+} else if(true){
+    console.log('I am inside ELSE IF Block')
+}   else {
+    console.log('I am inside ELSE Block')
+}
