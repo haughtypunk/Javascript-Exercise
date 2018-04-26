5d73d052e9934c1073e0c282e909840fc28d3e46
haughtypunk <kevin.xarro@gmail.com>
2018-04-26 17:11:48 +0530

    Boolean data type

---
+++ 01Basics/nullAndBool.js
@@ -0,0 +1,12 @@
+
+//Grade system
+
+// 10 - Great Job
+// 5 - Work Harder
+// 2 - Poor
+// 0 - Die
+
+//Boolean - true/false 
+let actualMarks = 7
+let myGrade = (actualMarks == 10)
+console.log(myGrade)
